3979662a34e57b93085616181eb39ea6f161e032
Ahmad Sharif <theahmadsharif@gmail.com>
2024-04-08 01:43:29 +0300

    8 April

---
+++ Others/muse_dependency_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt update -y
+apt install python3-pip -y
+pip install pylsl
+pip install muselsl
+pip install mne
+apt install python3-tk -y
+apt install python3-pil python3-pil.imagetk -y
+apt install cmake
+apt install openssh-server -y
+apt-get install qt6-base-dev freeglut3-dev
+apt install build-essential g++ cmake
+apt install git -y
+apt install curl -y

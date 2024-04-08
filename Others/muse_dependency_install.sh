8cafe2f76ff82184a7708b75cd612fa0c7225d23
Ahmad Sharif <theahmadsharif@gmail.com>
2024-04-08 09:10:42 +0300

    8 April

--- Others/muse_dependency_install.sh
+++ Others/muse_dependency_install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+set -e -x
+handle_error() {
+    echo "An error occurred on line $1"
+    exit 1
+}
+
+trap 'handle_error $LINENO' ERR
+
+
+
 apt update -y
 apt install python3-pip -y
 pip install pylsl
@@ -6,9 +16,24 @@ pip install muselsl
 pip install mne
 apt install python3-tk -y
 apt install python3-pil python3-pil.imagetk -y
-apt install cmake
+echo "_____ Python and muselsl dependency Installation complete _____"
+
+apt install build-essential g++ cmake -yping
+apt install cmake -y
+echo "_____ Cmake Installation complete _____"
 apt install openssh-server -y
-apt-get install qt6-base-dev freeglut3-dev
-apt install build-essential g++ cmake
+echo "_____ openssh Installation complete _____"
+
+apt-get install qt6-base-dev freeglut3-dev -y
+
 apt install git -y
 apt install curl -y
+
+
+curl -L -O https://github.com/alexandrebarachant/muse-lsl/archive/master.zip
+unzip master -d ~/Desktop/
+rm master.zip
+
+curl -L -O https://github.com/sccn/liblsl/archive/master.zip
+unzip master -d ~/Desktop/
+rm master.zip
